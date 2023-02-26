ded6f64850e4646d3682cf91cc394eafeb675dc4
Dinh-nguyen1612 <124942296+Dinh-nguyen1612@users.noreply.github.com>
2023-02-26 23:07:42 +0700

    Add files via upload

---
+++ book.java
@@ -0,0 +1,30 @@
+
+package bt;
+
+public class book {
+    public String boCode;
+    public String Title;
+    public String boAuthor;
+    public book(){}
+
+    public book(String boCode, String Title, String boAuthor)
+    {
+        this.boCode = boCode;
+        this.Title = Title;
+        this.boAuthor = boAuthor;
+    }
+
+    public String getBoCode() {
+        return boCode;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getBoAuthor() {
+        return boAuthor;
+    }
+
+    
+}
